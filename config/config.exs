6fe508ec218ea74e5fa1512a18f9499927c4eacb
mirenk0 <MirkoDSov@gmail.com>
2025-04-18 12:52:33 +0200

    update secrets

--- config/config.exs
+++ config/config.exs
@@ -12,6 +12,10 @@ import Config
 config :logger,
   level: :info
 
+if File.exists?("config/secrets.exs") do
+  import_config("secrets.exs")
+end
+
 # Sample configuration:
 #
 #     config :logger, :console,
