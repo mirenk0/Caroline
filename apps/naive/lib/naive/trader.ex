ec7baff178843cbe921ce5794aa3a69de919324b
mirenk0 <MirkoDSov@gmail.com>
2025-04-18 12:30:51 +0200

    start naive trader implementation

---
+++ apps/naive/lib/naive/trader.ex
@@ -0,0 +1,35 @@
+defmodule Naive.Trader do
+  use GenServer
+
+  require Logger
+
+  def start_link(%{} = args) do
+    GenServer.start_link(__MODULE__, args, name: :trader)
+  end
+
+  def init(%{symbol: symbol, profit_interval: profit_interval}) do
+    symbol = String.upcase(symbol)
+
+    Logger.info("Initializing new trader for #{symbol}")
+
+    tick_size = fetch_tick_size(symbol)
+
+    {:ok,
+     %State{
+       symbol: symbol,
+       profit_interval: profit_interval,
+       tick_size: tick_size
+     }}
+  end
+end
+
+defmodule State do
+  @enforce_keys [:symbol, :profit_interval, :tick_size]
+  defstruct [
+    :symbol,
+    :buy_order,
+    :sell_order,
+    :profit_interval,
+    :tick_size
+  ]
+end
