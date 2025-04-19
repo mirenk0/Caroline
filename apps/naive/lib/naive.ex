2db0ff10438efa19e4827aa3f450caf004982f13
mirenk0 <MirkoDSov@gmail.com>
2025-04-19 13:39:56 +0200

    add interface to naive

--- apps/naive/lib/naive.ex
+++ apps/naive/lib/naive.ex
@@ -3,4 +3,14 @@ defmodule Naive do
   Documentation for `Naive`.
   """
   alias Streamer.Binance.TradeEvent
+
+  def start_trading(symbol) do
+    symbol = String.upcase(symbol)
+
+    {:ok, _pid} =
+      DynamicSupervisor.start_child(
+        Naive.DynamicSymbolSupervisor,
+        {Naive.SymbolSupervisor, symbol}
+      )
+  end
 end
