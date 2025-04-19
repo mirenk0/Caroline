488f51c5a9ac6c79ebdccac063f332b5ff290280
mirenk0 <MirkoDSov@gmail.com>
2025-04-19 13:02:09 +0200

    add binance_mock

---
+++ apps/binance_mock/lib/binance_mock.ex
@@ -0,0 +1,75 @@
+defmodule BinanceMock do
+  use GenServer
+
+  alias Decimal, as: D
+  alias Streamer.Binance.TradeEvent
+
+  require Logger
+
+  defmodule State do
+    defstruct order_books: %{}, subscriptions: [], fake_order_id: 1
+  end
+
+  defmodule OrderBook do
+    defstruct buy_side: [], sell_side: [], historical: []
+  end
+
+  def start_link(_args) do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, %State{}}
+  end
+
+  def get_exchange_info do
+    Binance.get_exchange_info()
+  end
+
+  def order_limit_buy(symbol, quantity, price, "GTC") do
+    order_limit(symbol, quantity, price, "BUY")
+  end
+
+  def order_limit_sell(symbol, quantity, price, "GTC") do
+    order_limit(symbol, quantity, price, "SELL")
+  end
+
+  defp order_limit(symbol, quantity, price, side) do
+    %Binance.Order{} =
+      fake_order =
+      generate_fake_order(
+        symbol,
+        quantity,
+        price,
+        side
+      )
+
+    GenServer.cast(
+      __MODULE__,
+      {:add_order, fake_order}
+    )
+
+    {:ok, convert_order_to_order_response(fake_order)}
+  end
+
+  defp subscribe_to_topic(symbol, subscriptions) do
+    symbol = String.upcase(symbol)
+    stream_name = "TRADE_EVENTS:#{symbol}"
+
+    case Enum.member?(subscriptions, symbol) do
+      false ->
+        Logger.debug("BinanceMock subscribing to #{stream_name}")
+
+        Phoenix.PubSub.subscribe(
+          Streamer.PubSub,
+          stream_name
+        )
+
+        [symbol | subscriptions]
+
+      _ ->
+        subscriptions
+    end
+  end
+end
+
